45a58b655715ae2253cd10451ff3f9fda6c2abc6
Asim1996 <awesomeasim2596@gmail.com>
2018-04-05 00:57:43 +0530

    responsive

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -5,10 +5,12 @@ const OptionModal = (props) =>(
 		isOpen={!!props.selectedOption}
 		onRequestClose={props.clearOption}
 		contentLabel="Selected Options"	
+		closeTimeoutMS={200}
+		className="modal"
 		>
-			<h3>Selected Option</h3>
-			{props.selectedOption && <p>{props.selectedOption}</p>}
-			<button onClick={props.clearOption}>Okay</button>
+			<h3 className="modal__title">Selected Option</h3>
+			{props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+			<button className="button" onClick={props.clearOption}>Okay</button>
 		</Modal>
 		);
 export default OptionModal;
